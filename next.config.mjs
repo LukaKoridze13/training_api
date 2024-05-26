d4b026ceadc289c34094df42767a98088ea0fbba
Luka Koridze <lukakoridze13@gmail.com>
2024-05-26 21:11:59 +0400

    'deploy'

--- next.config.mjs
+++ next.config.mjs
@@ -14,12 +14,20 @@ const nextConfig = {
             value: "*",
           },
           {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
-          },
-          {
             key: "Content-Type",
             value: "application/json",
+          },
+          {
+            key: "Allow",
+            value: "GET, POST, OPTIONS, DELETE",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, OPTIONS, DELETE",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Authorization, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
           },
         ],
       },
