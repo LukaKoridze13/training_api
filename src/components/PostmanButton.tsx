ef06e63069461fff12fb2b96490c1a33c8d754b5
Luka Koridze <lukakoridze13@gmail.com>
2024-05-26 22:24:01 +0400

    'deploy'

--- src/components/PostmanButton.tsx
+++ src/components/PostmanButton.tsx
@@ -30,6 +30,7 @@ const PostmanButton = () => {
       data-postman-visibility="public"
       data-postman-var-1="23546965-a2c8c7d6-46ef-44fa-b717-d376a6f033dd"
       data-postman-collection-url="entityId=23546965-a2c8c7d6-46ef-44fa-b717-d376a6f033dd&entityType=collection&workspaceId=9591de5b-77ae-40e2-84b5-6bbbdd2c877a"
+      data-postman-param="env%5BTraining%20API%20ENV%5D=W3sia2V5IjoiQkFTRV9VUkwiLCJ2YWx1ZSI6Imh0dHA6Ly9sb2NhbGhvc3Q6MzAwMC9hcGkiLCJlbmFibGVkIjp0cnVlLCJ0eXBlIjoiZGVmYXVsdCIsInNlc3Npb25WYWx1ZSI6Imh0dHBzOi8vdHJhaW5pbmctYXBpLXRocmVlLnZlcmNlbC5hcHAvYXBpIiwic2Vzc2lvbkluZGV4IjowfSx7ImtleSI6IkFVVEhfVVJMIiwidmFsdWUiOiJ7e0JBU0VfVVJMfX0vbG9naW4iLCJlbmFibGVkIjp0cnVlLCJ0eXBlIjoiZGVmYXVsdCIsInNlc3Npb25WYWx1ZSI6Int7QkFTRV9VUkx9fS9sb2dpbiIsInNlc3Npb25JbmRleCI6MX0seyJrZXkiOiJMQU5HVUFHRSIsInZhbHVlIjoia2EiLCJlbmFibGVkIjp0cnVlLCJ0eXBlIjoiZGVmYXVsdCIsInNlc3Npb25WYWx1ZSI6ImthIiwic2Vzc2lvbkluZGV4IjoyfV0="
     ></div>
   );
 };
